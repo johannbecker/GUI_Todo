b690281f79db35fe2489ffa32657e70f685cd0e2
johannbecker <lilli.johann.becker@gmail.com>
2014-02-20 09:27:27 +0100

    Menu hinzugefügt

--- GUI_Todo/src/de/i3a/todo/Todo.java
+++ GUI_Todo/src/de/i3a/todo/Todo.java
@@ -6,9 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,6 +28,8 @@ public class Todo extends JFrame {
 	private JButton jbtnEintragen;
 	private JPanel jpanel;
 	private JScrollPane jscrollPane;
+	private JMenuBar jmenu;
+	private JMenuItem jmiSpeichern, jmiBeenden, jmiUeber;
 	
 	public Todo() {
 		
@@ -32,10 +38,29 @@ public class Todo extends JFrame {
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
+		createMenu();
 		initComponents();
 		initEvents();
-		
+		this.setJMenuBar(jmenu);
 		this.setVisible(true);		
+	}
+
+	private void createMenu() {
+		jmenu = new JMenuBar();
+		
+		JMenu jmenuDatei = new JMenu("Datei");
+		JMenu jmenuHilfe = new JMenu("Hilfe");
+
+		jmiSpeichern = new JMenuItem("Speichern");
+		jmiBeenden = new JMenuItem("Beenden");
+		jmiUeber = new JMenuItem("Über");
+
+		jmenuDatei.add(jmiSpeichern);
+		jmenuDatei.add(jmiBeenden);
+		jmenuHilfe.add(jmiUeber);
+		
+		jmenu.add(jmenuDatei);
+		jmenu.add(jmenuHilfe);
 	}
 
 	private void initComponents() {
@@ -71,15 +96,46 @@ public class Todo extends JFrame {
 		});
 		
 		this.addWindowListener(new WindowAdapter() {
+
 			@Override
 			public void windowClosing(WindowEvent e) {
-				int i = JOptionPane.showConfirmDialog(Todo.this, "Wollen Sie wirlich beenden?", "Beenden?", JOptionPane.YES_NO_OPTION);
-				if (i==JOptionPane.YES_OPTION) {
-					System.exit(NORMAL);
-				}
-			}
+				beenden();
+			}			
+		});		
+				
+				
+		jmiSpeichern.addActionListener(new ActionListener() {
 			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Es wird gespeichert.");
+			}
 		});
+		
+		jmiBeenden.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				beenden();
+			}
+
+		});
+		
+		jmiUeber.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String infoText = "Version 0.0.1\nJohann Becker";
+				JOptionPane.showMessageDialog(null, infoText);
+			}
+		});
+	}
+	
+	private void beenden() {
+		int i = JOptionPane.showConfirmDialog(Todo.this, "Wollen Sie wirlich beenden?", "Beenden?", JOptionPane.YES_NO_OPTION);
+		if (i==JOptionPane.YES_OPTION) {
+			System.exit(NORMAL);
+		}
 	}
 	
 	private void eingabe(){
